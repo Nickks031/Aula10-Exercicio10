5d583e99c2f5634d39e28841c622b38131c608b4
Nicolas Ribeiro <nicolassthoker@gmail.com>
2026-05-18 22:54:37 -0300

    O Aluno

---
+++ Aula10/Aluno/src/Aluno.java
@@ -0,0 +1,26 @@
+// Nome: Nicolas
+// 1222010390
+
+public class Aluno {
+    // Atributos
+    public String nome;
+    public String matricula;
+    public double nota1;
+    public double nota2;
+
+    // Métodos
+    public double calcularMedia() {
+        return (this.nota1 + this.nota2) / 2.0;
+    }
+
+    public boolean aprovado() {
+        return this.calcularMedia() >= 6.0;
+    }
+
+    public void exibirBoletim() {
+        System.out.println("Aluno: " + this.nome + " | Matrícula: " + this.matricula);
+        System.out.println("Nota 1: " + this.nota1 + " | Nota 2: " + this.nota2);
+        System.out.printf("Média: %.2f%n", this.calcularMedia());
+        System.out.println("Situação: " + (this.aprovado() ? "APROVADO" : "REPROVADO"));
+    }
+}
---
+++ Aula10/Aluno/src/App.java
@@ -0,0 +1,14 @@
+// Nome: Nicolas
+// Matrícula: [Sua Matrícula Aqui]
+
+public class App {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        aluno.nome = "Maria Clara";
+        aluno.matricula = "2024001";
+        aluno.nota1 = 7.5;
+        aluno.nota2 = 8.0;
+
+        aluno.exibirBoletim();
+    }
+}
